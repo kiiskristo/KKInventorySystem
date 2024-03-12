97275ed8b175441fe5deb97dd149e4eaa763bd5d
Kristo Kiis <kiiskristo@gmail.com>
2024-03-11 20:44:22 -0400

    form

--- KKInventorySystem/KKInventorySystem/Form1.Designer.cs
+++ KKInventorySystem/KKInventorySystem/Form1.Designer.cs
@@ -28,13 +28,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.partsSearchButton = new System.Windows.Forms.Button();
+            this.partsSearchInput = new System.Windows.Forms.TextBox();
+            this.productsSearchInput = new System.Windows.Forms.TextBox();
+            this.productsSearchButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(16, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(265, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory Management System";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(12, 46);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Panel1.Controls.Add(this.partsSearchInput);
+            this.splitContainer1.Panel1.Controls.Add(this.partsSearchButton);
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView2);
+            this.splitContainer1.Panel2.Controls.Add(this.productsSearchInput);
+            this.splitContainer1.Panel2.Controls.Add(this.productsSearchButton);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Size = new System.Drawing.Size(776, 392);
+            this.splitContainer1.SplitterDistance = 386;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label2.Location = new System.Drawing.Point(15, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Parts";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.label3.Location = new System.Drawing.Point(15, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Products";
+            // 
+            // partsSearchButton
+            // 
+            this.partsSearchButton.Location = new System.Drawing.Point(306, 15);
+            this.partsSearchButton.Name = "partsSearchButton";
+            this.partsSearchButton.Size = new System.Drawing.Size(67, 23);
+            this.partsSearchButton.TabIndex = 1;
+            this.partsSearchButton.Text = "Search";
+            this.partsSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // partsSearchInput
+            // 
+            this.partsSearchInput.Location = new System.Drawing.Point(136, 16);
+            this.partsSearchInput.Name = "partsSearchInput";
+            this.partsSearchInput.Size = new System.Drawing.Size(156, 20);
+            this.partsSearchInput.TabIndex = 2;
+            // 
+            // productsSearchInput
+            // 
+            this.productsSearchInput.Location = new System.Drawing.Point(128, 15);
+            this.productsSearchInput.Name = "productsSearchInput";
+            this.productsSearchInput.Size = new System.Drawing.Size(156, 20);
+            this.productsSearchInput.TabIndex = 4;
+            // 
+            // productsSearchButton
+            // 
+            this.productsSearchButton.Location = new System.Drawing.Point(301, 14);
+            this.productsSearchButton.Name = "productsSearchButton";
+            this.productsSearchButton.Size = new System.Drawing.Size(67, 23);
+            this.productsSearchButton.TabIndex = 3;
+            this.productsSearchButton.Text = "Search";
+            this.productsSearchButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 61);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(354, 222);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(19, 61);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(354, 222);
+            this.dataGridView2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox partsSearchInput;
+        private System.Windows.Forms.Button partsSearchButton;
+        private System.Windows.Forms.TextBox productsSearchInput;
+        private System.Windows.Forms.Button productsSearchButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
